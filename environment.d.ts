be26cdb321819300f7ba3f3aea58efab042b43f2
Rafał Ciok <ravciok@gmail.com>
2023-11-02 14:00:57 +0100

    update example env

--- environment.d.ts
+++ environment.d.ts
@@ -1,6 +1,7 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
-    readonly NODE_ENV: 'development' | 'production' | 'test'
+    readonly NODE_ENV: 'development' | 'production'
+    readonly REDIS_URL: string
     readonly NEXT_PUBLIC_AUTH0_DOMAIN: string
     readonly NEXT_PUBLIC_AUTH0_CLIENT_ID: string
     readonly NEXT_PUBLIC_BASE_URL: string
